42c3999678ffdfe6f7078cad05abf5d273162e9d
Jesse B. Miller <jesse@jessebmiller.com>
2025-02-25 23:21:35 -0600

    keybinding for rofi

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,11 @@
 import XMonad
+import XMonad.Util.EZConfig
 
-main = xmonad def 
+main = xmonad $ def 
   { terminal    = "/run/current-system/sw/bin/alacritty"
   , modMask     = mod4Mask
   , borderWidth = 1
   }
+  `additionalKeys`
+  [ ((mod1Mask, xK_p), spawn "rofi")
+  ]
