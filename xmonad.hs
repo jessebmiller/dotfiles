40d093ee709ed48a27c486ee5dc0b6992da2f532
Jesse Miller <jesse@jessebmiller.com>
2025-03-12 01:08:22 -0500

    switch

--- xmonad.hs
+++ xmonad.hs
@@ -9,6 +9,9 @@ main = xmonad $ def
   `additionalKeys`
   [ ((mod4Mask, xK_p), spawn "ulauncher-toggle")
   , ((mod4Mask, xK_u), spawn "dm-tool switch-to-greeter")
+  , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+  , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
+  , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
   , ((0, xK_F1), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
   , ((0, xK_F2), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
   , ((0, xK_F3), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
